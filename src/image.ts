4641cd94456fa61969c82c493a738ce2ca71165c
Anton Panchenko <apanchenko@gmail.com>
2020-01-25 02:56:22 +0300

    Add fancy comments

--- src/image.ts
+++ src/image.ts
@@ -1,3 +1,10 @@
+/**
+ * Image represents a source or target folder
+ * to compare and reflect
+ */
 export interface Image {
+  /**
+   * Add file
+   */
   onFile(name: string) : void;
 }
--- src/walker.ts
+++ src/walker.ts
@@ -9,12 +9,15 @@ export class Walker {
    */
   async list(path:string, image:Image) {
     const stats = fs.statSync(path);
+    /* report file to image */
     if (stats.isFile()) {
       image.onFile(path);
     }
+    /* walk directory */
     else if (stats.isDirectory()) {
       await this.walkDir(path, image);
     }
+    /* report unknown entity */
     else {
       throw new Error(`Unknown object ${name}`);
     }
@@ -29,12 +32,15 @@ export class Walker {
     for (const dirent of dirents) {
       /* prepare entity name */
       const name = path.join(dir, dirent.name);
+      /* report file to image */
       if (dirent.isFile()) {
         image.onFile(name);
       }
+      /* walk another dir */
       else if (dirent.isDirectory()) {
         this.walkDir(name, image);
       }
+      /* report unknown entity */
       else {
         throw new Error(`Unknown object ${name}`);
       }
